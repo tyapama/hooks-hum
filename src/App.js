7c3444701938d5d68e700801f9be94fa5f2429aa
tyapama@yahoo.co.jp <tyapama@yahoo.co.jp>
2020-05-21 10:45:57 +0900

    implement useState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 const App = () =>{
+  // setStateで初期値をしてでき、状態と状態を変更できる関数を返す
+  const [count,setCount]=useState(0)
+
+  const increment =()=>setCount(count+1)
+  const decrement =()=>setCount(count-1)
+
+  // setCountの中には前の状態を引き継げる
+  const increment2 =()=>setCount(previousCount=>previousCount+1)
+  const decrement2 =()=>setCount(previousCount=>previousCount+1)
+
+  const reset =()=>setCount(0)
+  const double =()=>setCount(previousCount=>previousCount*2)
+  const devide =()=>setCount(previousCount=>previousCount/2)
+
   return (
 <>
-template
+<div>count:{count}</div>
+{/* {}にはjsxでJSが記載できる */}
+<div>
+<button onClick={increment}>+1</button>
+<button onClick={decrement}>-1</button>
+</div>
+<div>
+<button onClick={increment2}>+1</button>
+<button onClick={decrement2}>-1</button>
+</div>
+<div>
+<button onClick={double}>x2</button>
+<button onClick={devide}>÷2</button>
+</div>
+<div>
+  <button onClick={reset}>reset</button>
+</div>
 </>
   );
 }
